f9c12dca6a01bd1bd7eb3b754aeea499f7dbcbfd
symnoisy <symnoisy@gmail.com>
2021-08-17 23:12:07 +0900

    kotlin 리스트와 딕셔너리 사용법

--- kotlin_worksheet.ws.kts
+++ kotlin_worksheet.ws.kts
@@ -2,3 +2,12 @@
 Script Test
 """.trimIndent()
 
+
+// python list 랑 똑같음
+val list = listOf("a", "b", "c")
+println(list)
+
+// mapOf는 python dictionary 역할
+val map = mapOf("A" to 1, "b" to 2, "C" to 3)
+println(map)
+println(map["A"])
